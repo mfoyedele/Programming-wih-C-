8788e297c555dc6bdbbb90876407dfeac9b66b39
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-11-01 08:25:00 +0000

    Add files via upload

---
+++ C# Fundamentals i/ChapterOne/QuestionOne/QuestionTwo/QuestionTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints 1, 101, 1001, each on a new line
+            int one = 1;
+            int oneZeroOne = 101;
+            int oneZeroZeroOne = 1001;
+
+            Console.WriteLine(one);
+            Console.WriteLine();
+            Console.WriteLine(oneZeroOne);
+            Console.WriteLine();
+            Console.WriteLine(oneZeroZeroOne);
+            Console.WriteLine();
+            // OR
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
