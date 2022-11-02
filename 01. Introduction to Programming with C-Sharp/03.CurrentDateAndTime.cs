87e72e94965165d0a488e27f9ef30dcf6c04b77c
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-11-02 14:00:12 +0100

    Add files via upload

---
+++ 01. Introduction to Programming with C-Sharp/03.CurrentDateAndTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that prints the current Date and Time.
+            Console.WriteLine("Current Date and Time is : ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now); 
+            // OR
+            Console.WriteLine("Current Date and Time is : " + System.DateTime.Now);
+        }
+    }
+}
---
+++ 01. Introduction to Programming with C-Sharp/04.SquareRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write the square root of 12345
+            Console.WriteLine("The Square Root of 12345 is : " + Math.Sqrt(12345));
+            Console.WriteLine();
+        }
+    }
+}
